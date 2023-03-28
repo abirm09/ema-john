7fd06b4eac7a3755ccf30b9748615c2c346bc2a9
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-03-28 14:29:20 +0600

    commit for new feature

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,18 +4,21 @@ import { addition } from "../../utilities/Addition";
 const Cart = ({ cart }) => {
   //   const totalPrice = addition(cart, "price");
   //   const totalShipping = addition(cart, "shipping");
+  console.log(cart);
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = parseFloat(((totalPrice * 7) / 100).toFixed(2));
   const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="space-y-5">
       <h2 className="text-center font-semibold text-2xl">Order summery</h2>
-      <h3>Selected item : {cart.length}</h3>
+      <h3>Selected item : {quantity}</h3>
       <p>Total price : ${totalPrice}</p>
       <p>Total shipping : ${totalShipping}</p>
       <p>Tax : ${tax}</p>
